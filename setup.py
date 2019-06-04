fe907b4da968cda5e157bf3f23d85d827964bd10
bundickm <bundickm@gmail.com>
2019-06-03 23:01:53 -0700

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,30 @@
+'''
+eda - a package for quick exploratory analysis on all features in a dataframe
+'''
+import setuptools
+
+required = [
+    'numpy',
+    'pandas'
+]
+
+with open('README.md','r') as fh:
+    LONG_DESCRIPTION = fh.read()
+
+setuptools.setup(
+    name = 'eda',
+    version = '0.0.1',
+    author = 'bundickm',
+    description = 'A package for quick exploratory analysis on all features in a dataframe',
+    long_description = LONG_DESCRIPTION,
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/bundickm/eda',
+    packages = setuptools.find_packages(),
+    python_requires = '>= 3.5',
+    install_requires = REQUIRED,
+    classifier = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+)
