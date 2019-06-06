3616c94aa75640b088571ed47a5100c385e18195
bundickm <bundickm@gmail.com>
2019-06-06 13:58:33 -0700

    added boxplot function

--- walkabout/plot.py
+++ walkabout/plot.py
@@ -7,14 +7,14 @@ from matplotlib.colors import LinearSegmentedColormap
 
 
 __all__ = ['univariate_distribution', 'bivariate_categorical_distribution',
-           'probability', 'correlation_heatmap', 'null_correlation_heatmap',
+           'residuals', 'boxplot', 'correlation_heatmap', 'null_correlation_heatmap',
            'missingness_map']
 
 
 def univariate_distribution(df, cols=5, figsize=(20,15),
                             hspace=0.5, wspace=0.5):
     '''
-    plot the distribution of all features in a dataframe
+    Plot the distribution of all features in a dataframe
     original function found here:
     https://github.com/dformoso/sklearn-classification/blob/master/Data%20Science%20Workbook%20-%20Census%20Income%20Dataset.ipynb
 
@@ -114,6 +114,47 @@ def residuals(df, target, cols=3, figsize=(10, 15), hspace=1, wspace=1):
         ax = fig.add_subplot(rows, cols, i + 1)
         sns.residplot(X[column], y, line_kws=dict(color='r'), lowess=True)
         ax.set_title(column)
+
+
+def boxplot(df, target=None, label_limit=10, figsize=(10,10), cols=3, wspace=.5, hspace=.5):
+    '''
+    Plot the box plot of all numeric features, or plot all features 
+    against a numeric feature.
+    
+    Input:
+    df: Pandas DataFrame object
+    target: string, default None. Name of the target numeric feature in df. 
+            If target == None then only numeric features will generate a 
+            boxplot
+    label_limit: int, default 10. The max number of labels in a feature before 
+                 it is excluded from the plotting of bivariate boxplots
+    cols: number of graphs to display per row
+    figsize: tuple of floats representing height and width of the plots
+    hspace: the amount of height reserved for space between subplots
+    wspace: the amount of width reserved for space between subplots
+
+    Output:
+    Display graphs to the screen
+    '''
+    # plot settings
+    fig = plt.figure(figsize=figsize)
+    fig.subplots_adjust(left=None, bottom=None, right=None, top=None,
+                        wspace=wspace, hspace=hspace)
+    rows = ceil(float(df.shape[1]) / cols)
+    
+    if target == None: #Univariate boxplot
+        numerics = df.select_dtypes(include='number').columns
+        for i, column in enumerate(numerics):
+            ax = fig.add_subplot(rows, cols, i + 1)
+            ax.set_title(column)
+            g = sns.boxplot(df[column])
+    else: #bivariate boxplot
+        columns = [col for col in df.columns 
+                   if ((df[col].nunique() < 10) and (col != target))]
+        for i, column in enumerate(columns):
+            ax = fig.add_subplot(rows, cols, i + 1)
+            ax.set_title(column)
+            g = sns.boxplot(df[column], df[target])
 
 
 def correlation_heatmap(df, figsize=(5, 5), annot=True):
