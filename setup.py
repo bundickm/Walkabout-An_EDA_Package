d4a4e2c5b7e840c390e1c2c4d18ecfaf54b0b59f
bundickm <bundickm@gmail.com>
2019-07-01 11:02:26 -0700

    version number

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ walkabout - a package for quick exploratory analysis on all features in a datafr
 '''
 import setuptools
 
-REQUIRED = [
-]
 
 with open('README.md','r') as fh:
     LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
     name = 'walkabout',
-    version = '0.0.12',
+    version = '0.0.13',
     author = 'bundickm',
     description = 'A package for quick exploratory analysis on all features in a dataframe',
     long_description = LONG_DESCRIPTION,
@@ -19,7 +17,6 @@ setuptools.setup(
     url = 'https://github.com/bundickm/walkabout',
     packages = setuptools.find_packages(),
     python_requires = '>= 3.5',
-    install_requires = REQUIRED,
     classifier = [
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
